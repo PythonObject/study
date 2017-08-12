207e334f2a2e3c84e160999d3e8c62b4a1e56c23
wumingming <694021065@qq.com>
2017-08-12 12:03:08 +0800

    none

--- multi_thread.py
+++ multi_thread.py
@@ -3,6 +3,30 @@ import threading
 import thread
 import time
 
+# """
+# When you use python manage.py runserver Django start two processes, one for the actual development server and other
+# to reload your application when the code change
+#
+# You can test it importing os inside your AppConfig class and print the process id inside the ready function like so:
+#
+# import os
+#
+# class SomeAppConfig(AppConfig):
+#     name = 'some_app'
+#
+#     def ready(self):
+#         print(os.getpid())
+#
+# You will see it prints two different processes
+#
+# You can also start the server without the reload option, and you will see only one process running (and your code
+# print("Redefined ready method in some_app") will only be executed once):
+#
+# python manage.py runserver --noreload
+#
+# https://stackoverflow.com/questions/37441564/redefinition-of-appconfig-ready
+# """
+
 
 # 为线程定义一个函数
 def print_time(thread_name, delay):
@@ -15,11 +39,13 @@ def print_time(thread_name, delay):
 
 class myThread(threading.Thread):
     def __init__(self, thread_id, name, counter):
+
         threading.Thread.__init__(self)
         # super(myThread, self).__init__(self)
         self.thread_id = thread_id
         self.name = name
         self.counter = counter
+        # lock.release()
 
     def run(self):
         # 从写run函数，增加自己的代码到run中
@@ -28,7 +54,18 @@ class myThread(threading.Thread):
         print 'Exiting' + self.name
 
 
-    #创建两个线程
+def start_thread():
+    thread1 = myThread(1, 'Thread_1', 1)
+    thread2 = myThread(2, 'Thread_2', 2)
+    thread1.start()
+    thread2.start()
+
+
+class my_thread():
+    def __int__(self):
+        start_thread().clone()
+
+
 if __name__ == '__main__':
     # 使用简单线程接口创建线程
     # print 'multi_progress test function'
@@ -41,12 +78,15 @@ if __name__ == '__main__':
     # while 1:
     #     pass
 
-    #使用线程类创建和管理线程
-    thread1 = myThread(1, 'Thread_1', 1)
-    thread2 = myThread(2, 'Thread_2', 2)
-    thread1.start()
-    thread2.start()
-    # print threading.current_thread
+    # 使用线程类创建和管理线程
 
+    str = 'test'
+    print str.upper()
+    # print threading.current_thread
+    start_thread()
+    # start_thread()
+    # a = my_thread()
+    # b = my_thread()
     while True:
+
         pass
